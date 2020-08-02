79193a75146ce06d99e9f1fcffcee3cd29ae1424
Julian Paas <julian.paas@osaaru.com>
2020-08-02 18:50:47 -0400

    Trying CloudFrontWebDistribution

--- devops/lib/app-stack.ts
+++ devops/lib/app-stack.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Certificate, CertificateValidation } from "@aws-cdk/aws-certificatemanager"
-import { Distribution } from "@aws-cdk/aws-cloudfront"
+import { CloudFrontWebDistribution, SSLMethod } from "@aws-cdk/aws-cloudfront"
 import { S3Origin } from "@aws-cdk/aws-cloudfront-origins"
 import { ARecord, PublicHostedZone, RecordTarget } from "@aws-cdk/aws-route53"
 import { CloudFrontTarget } from "@aws-cdk/aws-route53-targets"
@@ -49,9 +49,15 @@ export class AppStack extends Stack {
       validation: CertificateValidation.fromDns(zone),
     })
 
-    const distribution = new Distribution(this, "CloudfrontDistribution", {
-      certificate,
-      defaultBehavior: { origin: new S3Origin(bucket, { originPath: "master" }) },
+    const distribution = new CloudFrontWebDistribution(this, "CloudfrontDistribution", {
+      aliasConfiguration: {
+        acmCertRef: certificate.certificateArn,
+        names: [hostName],
+      },
+      defaultRootObject: "index.html",
+      originConfigs: [
+        { behaviors: [{ isDefaultBehavior: true }], s3OriginSource: { originPath: "master", s3BucketSource: bucket } },
+      ],
     })
 
     const dnsRecord = new ARecord(this, "DnsRecord", {
