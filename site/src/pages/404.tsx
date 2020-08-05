c70b1ac9d84b81d078ecd86b137a1754694fa7d8
Julian Paas <julian.paas@osaaru.com>
2020-08-05 07:56:59 -0400

    Tries to fix flashing 404 page

--- site/src/pages/404.tsx
+++ site/src/pages/404.tsx
@@ -1,15 +1,20 @@
-import React from "react"
 import { Link } from "gatsby"
+import React from "react"
 import { Layout } from "../components/layout"
 import { SEO } from "../components/seo"
 
-const NotFoundPage: React.FC = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>Oops! 404</h1>
-    <h3>You just hit a route that doesn&#39;t exist.</h3>
-    <Link to="/">Go Home</Link>
-  </Layout>
-)
+// Prevents initial flash of the this page on initial render
+const browser = typeof window !== "undefined" && window
+
+const NotFoundPage: React.FC = () => {
+  return browser ? (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>Oops! 404</h1>
+      <h3>You just hit a route that doesn&#39;t exist.</h3>
+      <Link to="/">Go Home</Link>
+    </Layout>
+  ) : null
+}
 
 export default NotFoundPage
