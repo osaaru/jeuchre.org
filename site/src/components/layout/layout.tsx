f19f6a05023a01e0917b3422a20572c623e24b2d
Julian Paas <julian.paas@osaaru.com>
2020-08-16 09:53:25 -0400

    fix for flickering fonts

--- site/src/components/layout/layout.tsx
+++ site/src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { Helmet } from "react-helmet"
 import styled, { ThemeProvider } from "styled-components"
+import "typeface-diplomata-sc"
 
 import { GlobalStyles, theme } from "../../styles"
 import { CSSDebugger } from "../css-debugger"
@@ -20,9 +20,6 @@ const Layout: React.FC = ({ children }) => {
   return (
     <ThemeProvider theme={theme()}>
       <SEO />
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=Diplomata+SC&display=swap" rel="stylesheet" />
-      </Helmet>
       <GlobalStyles />
       <Header />
       <Container>
